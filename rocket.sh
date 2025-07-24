e54e28198207ae8f020de206de14c0bbff88c916
affe4ever <93663446+affe4ever@users.noreply.github.com>
2025-07-24 11:04:55 +0200

    Update rocket.sh

--- rocket.sh
+++ rocket.sh
@@ -7,15 +7,11 @@ frames=(
     "     |\n     |\n    / \\ \n   /___\\ \n   |   |  \n   |HELLO|  \n   |WORLD|  \n  /|___|\\ \n /_|___|_\\ \n"
     "\n\n\n\n\n   BLAST OFF! 🚀\n"
     )
-
-function clear_screen() {
-clear
-}
-
+    
 for frame in "${frames[@]}"
 do
-    clear_screen
+    clear
     echo -e "$frame"
     sleep 0.5
-clear_screen
+clear
 done
